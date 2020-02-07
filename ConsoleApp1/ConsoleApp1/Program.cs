b30858042e7c549ab0135caf31a00cca30c3c3c1
jzientar <j.a.zientar@gmail.com>
2020-02-06 20:31:02 -0400

    added class example

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Shape
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public virtual void Show()
+        {
+            //Console.WriteLine(string.Format("My name is {0} {1} shape", name));
+            Console.WriteLine($"my name is {Name} shape");
+        }
+
+    }
+
+    public class Square : Shape
+    {
+        public override void Show()
+        {
+            Console.WriteLine($"I'm a square {Name}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Shape myShape = new Shape("basic");
+            var myShape = new Shape() { Name = "basic", Color = "blue"};
+            myShape.Show();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
